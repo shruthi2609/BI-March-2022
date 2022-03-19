a76cd4a69e805bfbff978628b954822f313ccbb4
shruthi093 <jayasruthi.l@gmail.com>
2022-03-19 19:47:16 +0530

    19.03 js basics

---
+++ scripts/basics.js
@@ -0,0 +1,7 @@
+fname="Omkar"
+spec="MERN Stack"
+age=25
+year=1
+console.log(age+year)
+console.log(`Candidate ${fname} he is specilized in ${spec} he is ${age} young`)
+alert(`Welcome to BI ${fname}  ! happy learning `)
