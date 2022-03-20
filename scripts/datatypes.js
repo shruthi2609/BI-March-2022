da56e958e43ff7a6fedbb94995f996020ad02fa5
shruthi093 <jayasruthi.l@gmail.com>
2022-03-20 19:51:25 +0530

    20.03

---
+++ scripts/datatypes.js
@@ -0,0 +1,69 @@
+/*fname="john"
+console.log(fname)
+// let var const
+let fname
+fname="john#09"
+fname="peter"
+console.log(fname)
+const SALARY=30000
+//SALARY=40000 - > error 
+console.log(SALARY)
+let age="salary"
+let salary=30000.50
+console.log(typeof(age))
+console.log(age/2)
+console.log(age%5)
+console.log(age+salary)
+let bignumber=127981291820981209182819021n
+console.log(typeof bignumber)
+let fresher=0
+console.log(typeof fresher,fresher)
+let salary
+console.log(salary)
+console.log(-100/0)*/
+
+let users=["user1","user2","user3",9.0,23,["react js","js"]]
+console.log(users)
+let candidate={
+    fname:"peter",
+    age:23,
+    cgpa:9.0,
+    interest:["react js","js"]
+}
+console.log(candidate)
+let candidates={
+   student1: {
+        fname:"peter",
+        age:23,
+        cgpa:9.0,
+        interest:["react js","js"]
+    },
+   student2: {
+        fname:"john",
+        age:21,
+        cgpa:9.8,
+        interest:["react js","node js"]
+    }
+}
+let candidates_array=[
+    {
+        fname:"peter",
+        age:23,
+        cgpa:9.0,
+        interest:["react js","js"]
+    },
+   {
+        fname:"john",
+        age:21,
+        cgpa:9.8,
+        interest:["react js","node js"]
+    }
+]
+console.log(candidates)
+console.log(candidates_array)
+
+
+
+
+
+
---
+++ scripts/operators.js
@@ -0,0 +1,11 @@
+/*a=10
+console.log(a)
+console.log(a++)//a+1
+console.log(a)
+let experience=true
+experience=!experience
+console.log(experience)*/
+a=10
+b="10"
+console.log(a!==b)
+
